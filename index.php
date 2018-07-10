4747bdcfffc6fa05818c6a24ede319b31b48de91
Psudonem <30604357+Psudonem@users.noreply.github.com>
2018-07-10 12:26:24 -0400

    Fixed numerous errors, fully fuctional now
    -get request and string syntax repaired
    -gives error when no parameter is supplied

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-	$siteName=$_GET['yudosai'];
-	$apiRequest="https://neocities.org/api/info?sitename="+$siteName;
+	$test=false;
+	if(isset($_GET['site'])){
+		$siteName=$_GET['site'];
+		$apiRequest="https://neocities.org/api/info?sitename=".$siteName;
+		$test=true;
+	}
 	header("Content-type: image/png");
 	$imgWidth=88;
 	$imgHeight=31;
@@ -8,11 +12,14 @@
 	$black=imagecolorallocate($image, 0, 0, 0);
 	$blue=imagecolorallocate($image, 0, 255, 255);
 	$red=imagecolorallocate($image, 255, 255, 0);
-
-	$site_data=json_decode(file_get_contents("https://neocities.org/api/info?sitename=yudosai"),true);
-
-	imagestring($image, 1, 5, 5, "Hit Counter:", $red);
-	imagestring($image, 2, 5, 13, $site_data['info']['hits'], $blue);
+	if($test){
+		$site_data=json_decode(file_get_contents($apiRequest),true);
+		imagestring($image, 1, 5, 5, "Hit Counter:", $red);
+		imagestring($image, 2, 5, 13, $site_data['info']['hits'], $blue);
+	}else{
+		imagestring($image, 1, 5, 5, "no site found!", $red);
+	}
+	
 
 	imagepng($image);
 	imagedestroy($image);
